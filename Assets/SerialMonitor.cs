6d6816e894bbf943d8e4ae7b6bb02cc3269981aa
Eline <elinevooijs@outlook.com>
2024-01-25 09:29:51 +0100

    Open and close parameter

--- Assets/SerialMonitor.cs
+++ Assets/SerialMonitor.cs
@@ -8,30 +8,30 @@ using System.IO.Ports; // port access
 public class SerialMonitor : MonoBehaviour {
     SerialPort data_stream = new SerialPort ("/dev/cu.usbserial-0001", 115200); // arduino is connected to port "/dev/cu.usbserial-11210" at 9600 baudrate
     public string receivedstring;
-    public GameObject test_data;
-    public Rigidbody rb;
-    public float sensitivity = 0.01f;
-
     public string[] datas;
 
 
     private Animator m_Animator;
-    // public int tempValue;
+
+    bool flowerOpen = false;
 
     void Start() {
         data_stream.Open();
         m_Animator = GetComponentInChildren<Animator>(); // attach animator to gameobject we want to animate
     }
 
-    void Update() {
+    void Update() 
+    {
         if (m_Animator != null) {
             receivedstring = data_stream.ReadLine();
-            Debug.Log(receivedstring);
+            // Debug.Log(receivedstring); // data: CO2, TVOC, Temperature, Humidity, Sound
             string[] datas = receivedstring.Split(','); // split the data between the comma
-            Int32.TryParse(datas[0], out int tempValue);
-            // Debug.Log(tempValue);
-            if(tempValue == 0) {
-                m_Animator.SetTrigger("Open");
+            Int32.TryParse(datas[0], out int co2Value);
+            Debug.Log(co2Value);
+
+            if ((co2Value <= 419 && !flowerOpen) || (co2Value >= 420 && flowerOpen)) {
+                m_Animator.SetTrigger(co2Value <= 419 ? "Open" : "Close");
+                flowerOpen = !flowerOpen;
             }
         }
     }
